3b813eff9d8d15cacda67048d5d7c2f8f152b361
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-09 11:58:19 -0500

    Se agrego el while

--- Prog3.py
+++ Prog3.py
@@ -1,32 +1,34 @@
-print("Menu")
-print("Operaciones: ")
-print("S. Suma")
-print("R. Resta") 
-print("M. Mutiplicacion")
-print("D. Division")
-print("A. Salir")
+menu = "0"
+while menu == "0":
+    opcion = input("¿Que opcion eliges?: ").upper()
+    print("Menu")
+    print("Operaciones: ")
+    print("S. Suma")
+    print("R. Resta") 
+    print("M. Mutiplicacion")
+    print("D. Division")
+    print("A. Salir")
+   
+    num1 = int(input("Ingrese primer numero"))
+    num2 = int(input("Ingrese segundo numero"))
 
-opcion = input("¿Que opcion eliges?").upper()
-num1 = int(input("Ingrese primer numero"))
-num2 = int(input("Ingrese segundo numero"))
 
+    if opcion == "S":
+        result = num1 + num2
+        print(result)
 
-if opcion == "S":
-    result = num1 + num2
-    print(result)
+    elif opcion == "R":
+        result = num1 - num2
+        print(result)
 
-elif opcion == "R":
-    result = num1 - num2
-    print(result)
+    elif opcion == "M":
+        result = num1 * num2
+        print(result)
 
-elif opcion == "M":
-    result = num1 * num2
-    print(result)
+    elif opcion == "D":
+        result = num1/num2
+        print(result)
 
-elif opcion == "D":
-    result = num1/num2
-    print(result)
-
-elif opcion == "A":
-    print("saliendo del programa")
-
+    elif opcion == "A":
+        print("Saliendo del programa")
+        break
